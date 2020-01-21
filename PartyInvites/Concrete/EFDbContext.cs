6d5876eb4e8c5da2057f51a03d1aa5cbb4e8ded3
Jacob Lavarnway <lavarnwayj@thomas.edu>
2020-01-20 20:07:26 -0500

    added context class, connection string.

---
+++ PartyInvites/Concrete/EFDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using PartyInvites.Models;
+
+namespace PartyInvites.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+       public DbSet<GuestResponse> PartyInvitesTable { get; set; }
+    }
+}
